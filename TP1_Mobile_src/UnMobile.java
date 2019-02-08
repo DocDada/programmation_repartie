8fe7cc010255fdc2a15c37197b89fc2311da8913
Poulpy <paul.repain@yahoo.com>
2019-02-08 17:26:02 +0100

    TP1 Suite Mobile

--- TP1_Mobile_src/UnMobile.java
+++ TP1_Mobile_src/UnMobile.java
@@ -4,7 +4,8 @@ import javax.swing.*;
 class UnMobile extends JPanel implements Runnable
 {
     int saLargeur, saHauteur, sonDebDessin;
-    final int sonPas = 10, sonTemps=50, sonCote=40;
+    final int sonPas = 10, sonTemps=10, sonCote=40;
+    static private int p = 2;
     static private semaphoreGeneral mutex = new semaphoreGeneral(1);
 
     UnMobile(int telleLargeur, int telleHauteur)
@@ -17,10 +18,35 @@ class UnMobile extends JPanel implements Runnable
 
     public void run()
     {
+        double partie = 0.33;
         sonDebDessin = 0;
 
-        mutex.syncWait();
-        for (; sonDebDessin < saLargeur * 0.33; sonDebDessin++)
+        for (int j = 0; j != p-1; j++)
+            mutex.syncWait();
+
+        for (; ; sonDebDessin++)
+        {
+            repaint();
+            try {Thread.sleep(sonTemps);}
+            catch (InterruptedException telleExcp)
+                {telleExcp.printStackTrace();}
+
+            if (sonDebDessin > saLargeur * partie)
+            {
+                for (int j = 0; j != 3; j++)
+                    mutex.syncSignal();
+                if (partie == 0.33)
+                    partie = 0.66;
+                else
+                    partie = 0.33;
+                for (int j = 0; j != p; j++)
+                    mutex.syncWait();
+            }
+
+        }// for()
+
+        //mutex.syncSignal();
+        /*for (; sonDebDessin < saLargeur * 0.66; sonDebDessin++)
         {
             repaint();
             try {Thread.sleep(sonTemps);}
@@ -28,16 +54,7 @@ class UnMobile extends JPanel implements Runnable
                 {telleExcp.printStackTrace();}
 
         }// for()
-
-        mutex.syncSignal();
-        for (; sonDebDessin < saLargeur * 0.66; sonDebDessin++)
-        {
-            repaint();
-            try {Thread.sleep(sonTemps);}
-            catch (InterruptedException telleExcp)
-                {telleExcp.printStackTrace();}
-
-        }// for()
+        */
     }
 
     public void paintComponent(Graphics telCG)
