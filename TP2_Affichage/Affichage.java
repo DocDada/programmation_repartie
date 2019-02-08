c4c4ad37b350a40ed43e4d9cc5f2122bd165af35
Poulpy <paul.repain@yahoo.com>
2019-02-08 15:03:04 +0100

    Exercice 1 : utilisation de mutex, affichage de AAABB ou BBAAA

--- TP2_Affichage/Affichage.java
+++ TP2_Affichage/Affichage.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 import java.io.*;
 import java.io.InputStreamReader;
@@ -7,19 +7,26 @@ import java.io.Reader;
 import java.io.BufferedReader;
 import java.lang.String;
 
-public class Affichage extends Thread{
-	String texte; 
+public class Affichage extends Thread {
 
-	public Affichage (String txt){texte=txt;}
-	
-	public void run(){
+    String texte;
+    // pour que la variable/verrou soit accessible à toutes les
+    // instances de la classe
+    static semaphoreBinaire mutex = new semaphoreBinaire(0);
 
-		for (int i=0; i<texte.length(); i++){
-            // on affiche le texte, caractère par caractère
-            // section critique
-		    System.out.print(texte.charAt(i));
-		    try {sleep(100);} catch(InterruptedException e){};
-		}
+    public Affichage (String txt) {texte=txt;}
 
-	}
-}
+    public void run() {
+
+        synchronized(mutex) {
+            for (int i = 0; i < texte.length(); i++){
+                // on affiche le texte, caractère par caractère
+                // section critique
+                System.out.print(texte.charAt(i));
+                try { sleep(100); }
+                catch (InterruptedException e) {};
+            }
+        }// synchronized()
+    }// run()
+}// Thread
+
